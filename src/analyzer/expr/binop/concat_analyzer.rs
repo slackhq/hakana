2b1d5d27d084e981a95bc5953f23f7a89aa352f0
Matt Brown <github@muglug.com>
2022-10-10 15:10:42 -0400

    Flatten concat operations to prevent stack overflow

--- src/analyzer/expr/binop/concat_analyzer.rs
+++ src/analyzer/expr/binop/concat_analyzer.rs
@@ -2,11 +2,14 @@ use crate::expression_analyzer;
 use crate::scope_context::ScopeContext;
 use crate::statements_analyzer::StatementsAnalyzer;
 use crate::typed_ast::TastInfo;
-use hakana_reflection_info::t_atomic::TAtomic;
+use hakana_reflection_info::{
+    data_flow::{graph::GraphKind, node::DataFlowNode, path::PathKind},
+    t_atomic::TAtomic,
+    taint::SinkType,
+};
 use hakana_type::{get_string, wrap_atomic};
 use oxidized::aast;
-
-use super::arithmetic_analyzer::assign_arithmetic_type;
+use rustc_hash::FxHashSet;
 
 pub(crate) fn analyze<'expr, 'map, 'new_expr, 'tast>(
     statements_analyzer: &StatementsAnalyzer,
@@ -16,34 +19,92 @@ pub(crate) fn analyze<'expr, 'map, 'new_expr, 'tast>(
     tast_info: &'tast mut TastInfo,
     context: &mut ScopeContext,
 ) {
-    expression_analyzer::analyze(statements_analyzer, left, tast_info, context, &mut None);
-    expression_analyzer::analyze(statements_analyzer, right, tast_info, context, &mut None);
+    let mut concat_nodes = get_concat_nodes(left);
+    concat_nodes.push(right);
 
-    let left_expr_type = tast_info
-        .expr_types
-        .get(&(left.pos().start_offset(), left.pos().end_offset()));
-    let right_expr_type = tast_info
-        .expr_types
-        .get(&(left.pos().start_offset(), left.pos().end_offset()));
+    let mut all_literals = true;
 
-    let result_type =
-        if let (Some(left_expr_type), Some(right_expr_type)) = (left_expr_type, right_expr_type) {
-            if left_expr_type.all_literals() && right_expr_type.all_literals() {
-                wrap_atomic(TAtomic::TStringWithFlags(true, false, true))
-            } else {
-                get_string()
+    let decision_node = if let GraphKind::WholeProgram(_) = &tast_info.data_flow_graph.kind {
+        DataFlowNode::get_for_composition(statements_analyzer.get_hpos(stmt_pos))
+    } else {
+        DataFlowNode::get_for_variable_sink(
+            "composition".to_string(),
+            statements_analyzer.get_hpos(stmt_pos),
+        )
+    };
+
+    for (i, concat_node) in concat_nodes.iter().enumerate() {
+        expression_analyzer::analyze(
+            statements_analyzer,
+            concat_node,
+            tast_info,
+            context,
+            &mut None,
+        );
+
+        let expr_type = tast_info.expr_types.get(&(
+            concat_node.pos().start_offset(),
+            concat_node.pos().end_offset(),
+        ));
+
+        if let Some(expr_type) = expr_type {
+            all_literals = all_literals && expr_type.all_literals();
+
+            // if it's the last concat op in the chain we can remove some taints
+            let is_last = i == concat_nodes.len() - 1;
+
+            for (_, old_parent_node) in &expr_type.parent_nodes {
+                tast_info.data_flow_graph.add_path(
+                    old_parent_node,
+                    &decision_node,
+                    PathKind::Default,
+                    None,
+                    if is_last {
+                        Some(FxHashSet::from_iter([
+                            SinkType::HtmlAttributeUri,
+                            SinkType::CurlUri,
+                            SinkType::RedirectUri,
+                        ]))
+                    } else {
+                        None
+                    },
+                );
             }
         } else {
-            get_string()
-        };
+            all_literals = false;
+        }
+    }
+
+    let mut result_type = if all_literals {
+        wrap_atomic(TAtomic::TStringWithFlags(true, false, true))
+    } else {
+        get_string()
+    };
+
+    result_type
+        .parent_nodes
+        .insert(decision_node.get_id().clone(), decision_node.clone());
 
     // todo handle more string type combinations
-    assign_arithmetic_type(
-        statements_analyzer,
-        tast_info,
-        result_type,
-        left,
-        right,
-        stmt_pos,
-    );
+
+    tast_info.data_flow_graph.add_node(decision_node.clone());
+
+    tast_info.set_expr_type(&stmt_pos, result_type);
+}
+
+fn get_concat_nodes(expr: &aast::Expr<(), ()>) -> Vec<&aast::Expr<(), ()>> {
+    match &expr.2 {
+        aast::Expr_::Binop(x) => {
+            let (binop, e1, e2) = (&x.0, &x.1, &x.2);
+            match binop {
+                oxidized::ast_defs::Bop::Dot => {
+                    let mut concat_nodes = get_concat_nodes(e1);
+                    concat_nodes.push(e2);
+                    concat_nodes
+                }
+                _ => vec![expr],
+            }
+        }
+        _ => vec![expr],
+    }
 }
