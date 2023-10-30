6a636da0f6892c28a1fab81ab37863aabd564791
Matt Brown <github@muglug.com>
2023-10-30 14:08:53 -0400

    Update HHVM

--- src/code_info_builder/typehint_resolver.rs
+++ src/code_info_builder/typehint_resolver.rs
@@ -615,6 +615,7 @@ pub fn get_type_from_hint(
         Hint_::HfunContext(_) => panic!(),
         Hint_::Hvar(_) => panic!(),
         Hint_::Hrefinement(_, _) => panic!(),
+        Hint_::HclassArgs(_) => panic!(),
         Hint_::Hwildcard => TAtomic::TPlaceholder,
     };
 
