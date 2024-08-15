70cae4fde4d64373993f023ccdab4cb20902054f
Petr Pchelko <ppchelko@slack-corp.com>
2024-08-15 13:50:26 -0700

    Address comments and make the visitor a bit smarter

--- src/executable_code_finder/lib.rs
+++ src/executable_code_finder/lib.rs
@@ -1,16 +1,18 @@
-use std::sync::{Arc, Mutex};
-use std::time::Instant;
 use hakana_analyzer::config::Config;
 use hakana_logger::Logger;
 use hakana_reflection_info::code_location::FilePath;
+use hakana_reflection_info::file_info::ParserError;
 use hakana_str::{Interner, ThreadedInterner};
-use hakana_workhorse::file::{VirtualFileSystem};
-use hakana_workhorse::scanner::{add_builtins_to_scan};
+use hakana_workhorse::file::VirtualFileSystem;
+use hakana_workhorse::scanner::add_builtins_to_scan;
 use indicatif::{ProgressBar, ProgressStyle};
+use oxidized::aast::Stmt_;
+use oxidized::ast::Pos;
 use oxidized::{aast, aast_visitor::{visit, AstParams, Node, Visitor}};
 use rustc_hash::FxHashMap;
-use hakana_reflection_info::file_info::ParserError;
 use serde::Serialize;
+use std::sync::{Arc, Mutex};
+use std::time::Instant;
 
 #[derive(Debug, Serialize)]
 pub struct ExecutableLines {
@@ -24,12 +26,12 @@ pub fn scan_files(
     config: &Arc<Config>,
     threads: u8,
     logger: Arc<Logger>,
-) -> Result<Vec<ExecutableLines>,()> {
+) -> Result<Vec<ExecutableLines>, ()> {
     logger.log_debug_sync(&format!("{:#?}", scan_dirs));
 
     let mut files_to_scan = vec![];
     let mut files_to_analyze = vec![];
-    let mut interner=  Interner::default();
+    let mut interner = Interner::default();
     let existing_file_system = None;
 
     get_filesystem(
@@ -43,7 +45,6 @@ pub fn scan_files(
         &mut files_to_analyze,
     );
 
-    let invalid_files = Arc::new(Mutex::new(vec![]));
     let executable_lines = Arc::new(Mutex::new(vec![]));
 
     if !files_to_scan.is_empty() {
@@ -83,7 +84,6 @@ pub fn scan_files(
             let bar = bar.clone();
             let files_processed = files_processed.clone();
             let logger = logger.clone();
-            let invalid_files = invalid_files.clone();
             let executable_lines = executable_lines.clone();
             let root_dir = config.root_dir.clone();
 
@@ -91,21 +91,13 @@ pub fn scan_files(
                 let new_interner = ThreadedInterner::new(interner);
 
                 for file_path in &path_group {
-                    match scan_file(&new_interner, &root_dir, *file_path, &logger.clone(), ) {
-                        Err(_) => {
-                            invalid_files.lock().unwrap().push(*file_path);
-                        }
-                        Ok(res) => {
-                            let mut executable_lines = executable_lines.lock().unwrap();
-                            if !res.executable_lines.is_empty() {
-                                executable_lines.push(res);
-                            }
-                        }
-                    };
-
+                    let res = scan_file(&new_interner, &root_dir, *file_path, &logger.clone());
+                    let mut executable_lines = executable_lines.lock().unwrap();
+                    if !res.executable_lines.is_empty() {
+                        executable_lines.push(res);
+                    }
                     let mut tally = files_processed.lock().unwrap();
                     *tally += 1;
-
                     update_progressbar(*tally, bar.clone());
                 }
             });
@@ -128,11 +120,6 @@ pub fn scan_files(
             ));
         }
     }
-
-    let _invalid_files = Arc::try_unwrap(invalid_files)
-        .unwrap()
-        .into_inner()
-        .unwrap();
 
     Ok(Arc::try_unwrap(executable_lines).unwrap().into_inner().unwrap())
 }
@@ -181,7 +168,7 @@ pub(crate) fn scan_file(
     root_dir: &str,
     file_path: FilePath,
     logger: &Logger,
-) -> Result<ExecutableLines, ParserError>{
+) -> ExecutableLines {
     let interner = interner
         .parent
         .lock()
@@ -191,51 +178,94 @@ pub(crate) fn scan_file(
         .to_string();
 
     logger.log_debug_sync(&format!("scanning {}", str_path));
-
     let aast = hakana_workhorse::get_aast_for_path(file_path, &str_path);
-
     let aast = match aast {
         Ok(aast) => aast,
-        Err(err) => {
-            return Err(err);
-        }
+        Err(_) => panic!("invalid file: {}", str_path)
     };
-
-    let mut checker = Scanner {
-    };
-
-    let mut context= Vec::new();
+    let mut checker = Scanner {};
+    let mut context = Vec::new();
     match visit(&mut checker, &mut context, &aast.0) {
-        Ok(_) => {
-            Ok(ExecutableLines {
-                path: file_path.get_relative_path(&interner, root_dir),
-                executable_lines: context
-            })
-        }
-        Err(err) => {
-            Err(err)
-        }
+        Ok(_) => ExecutableLines {
+            path: file_path.get_relative_path(&interner, root_dir),
+            executable_lines: context,
+        },
+        Err(_) => panic!("invalid file: {}", str_path)
     }
-
 }
 
-struct Scanner {
-}
+struct Scanner {}
 
 impl<'ast> Visitor<'ast> for Scanner {
     type Params = AstParams<Vec<String>, ParserError>;
 
-    fn object(&mut self) -> &mut dyn Visitor<'ast, Params = Self::Params> {
+    fn object(&mut self) -> &mut dyn Visitor<'ast, Params=Self::Params> {
         self
     }
 
     fn visit_stmt(&mut self, c: &mut Vec<String>, p: &aast::Stmt<(), ()>) -> Result<(), ParserError> {
-        let result = p.recurse(c, self);
-        let start = p.0.to_raw_span().start.line();
-        let end = p.0.to_raw_span().end.line();
-        if start != 0 && end != 0 {
-            c.push(format!("{}-{}", start, end));
+        match &p.1 {
+            Stmt_::For(boxed) => {
+                push_start(&p.0, c); // The line where for loop is declared is coverable
+                boxed.1.recurse(c, self)
+            }
+            Stmt_::Foreach(boxed) => {
+                push_start(&p.0, c); // The line where foreach loop is declared is coverable
+                boxed.2.recurse(c, self)
+            }
+            Stmt_::Do(boxed) => {
+                push_pos(&boxed.1.1, c);
+                boxed.0.recurse(c, self)
+            }
+            Stmt_::While(boxed) => {
+                push_pos(&boxed.0.1, c);
+                boxed.1.recurse(c, self)
+            }
+            Stmt_::If(boxed) => {
+                push_pos(&boxed.0.1, c); // if expression
+                boxed.1.recurse(c, self)?;
+                boxed.2.recurse(c, self)
+            }
+            Stmt_::Switch(boxed) => {
+                // Skipping the switch statement, it's never covered by HHVM
+                for case_stmt in &boxed.1 {
+                    push_pos(&case_stmt.0.1, c);
+                    case_stmt.recurse(c, self)?;
+                }
+                boxed.2.recurse(c, self)
+            }
+            Stmt_::Block(boxed) => {
+                boxed.recurse(c, self)
+            }
+            Stmt_::Expr(boxed) => {
+                let start = boxed.1.to_raw_span().start.line();
+                let end = boxed.1.to_raw_span().end.line();
+                if start == end {
+                    c.push(format!("{}-{}", start, end));
+                } else {
+                    // Multi-line expressions seem to miss the first line in HHVM coverage
+                    c.push(format!("{}-{}", start + 1, end));
+                }
+                Ok(())
+            }
+            _ => {
+                let result = p.recurse(c, self);
+                push_pos(&p.0, c);
+                result
+            }
         }
-        result
+    }
+}
+
+fn push_start(p: &Pos, res: &mut Vec<String>) {
+    let start = p.to_raw_span().start.line();
+    res.push(format!("{}-{}", start, start));
+}
+
+fn push_pos(p: &Pos, res: &mut Vec<String>) {
+    let start = p.to_raw_span().start.line();
+    let end = p.to_raw_span().end.line();
+    if start != 0 && end != 0 {
+        res.push(format!("{}-{}", start, end));
     }
 }
