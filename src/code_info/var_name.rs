85115413222cd03c2ef31f6dda54b0b95f4f6398
Matt Brown <matthewbrown@slack-corp.com>
2025-05-19 15:08:18 -0400

    Add a maybe-more-efficient string representation

--- src/code_info/var_name.rs
+++ src/code_info/var_name.rs
@@ -1,4 +1,4 @@
-use std::borrow::Borrow;
+use std::borrow::{Borrow, Cow};
 use std::fmt::{Debug, Formatter};
 use std::hash::Hash;
 use std::ops::Deref;
@@ -6,25 +6,32 @@ use std::ops::Deref;
 #[derive(
     Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Default, serde::Serialize, serde::Deserialize,
 )]
-pub struct VarName(String);
-
-impl Debug for VarName {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "VarName({:?})", self.0)
-    }
-}
+pub struct VarName(compact_str::CompactString);
 
 impl VarName {
-    pub fn new(name: String) -> Self {
-        VarName(name)
+    #[inline]
+    pub fn empty() -> Self {
+        Self(compact_str::CompactString::default())
     }
 
-    pub fn to_string(&self) -> String {
-        self.0.clone()
+    #[inline]
+    pub fn new(name: impl AsRef<str>) -> Self {
+        Self(compact_str::CompactString::new(name))
+    }
+
+    #[inline]
+    pub const fn new_static(name: &'static str) -> Self {
+        Self(compact_str::CompactString::const_new(name))
     }
 
     pub fn as_str(&self) -> &str {
-        &self.0
+        self.0.as_str()
+    }
+}
+
+impl Debug for VarName {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Name({:?})", self.as_str())
     }
 }
 
@@ -69,5 +76,100 @@ impl<'a> From<&'a String> for VarName {
     #[inline]
     fn from(s: &'a String) -> Self {
         VarName(s.into())
+    }
+}
+
+impl<'a> From<Cow<'a, str>> for VarName {
+    #[inline]
+    fn from(cow: Cow<'a, str>) -> Self {
+        VarName(cow.into())
+    }
+}
+
+impl From<Box<str>> for VarName {
+    #[inline]
+    fn from(b: Box<str>) -> Self {
+        VarName(b.into())
+    }
+}
+
+impl From<compact_str::CompactString> for VarName {
+    #[inline]
+    fn from(value: compact_str::CompactString) -> Self {
+        Self(value)
+    }
+}
+
+impl From<VarName> for compact_str::CompactString {
+    #[inline]
+    fn from(name: VarName) -> Self {
+        name.0
+    }
+}
+
+impl FromIterator<char> for VarName {
+    fn from_iter<I: IntoIterator<Item = char>>(iter: I) -> Self {
+        Self(iter.into_iter().collect())
+    }
+}
+
+impl std::fmt::Display for VarName {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(self.as_str())
+    }
+}
+
+impl PartialEq<str> for VarName {
+    #[inline]
+    fn eq(&self, other: &str) -> bool {
+        self.as_str() == other
+    }
+}
+
+impl PartialEq<VarName> for str {
+    #[inline]
+    fn eq(&self, other: &VarName) -> bool {
+        other == self
+    }
+}
+
+impl PartialEq<&str> for VarName {
+    #[inline]
+    fn eq(&self, other: &&str) -> bool {
+        self.as_str() == *other
+    }
+}
+
+impl PartialEq<VarName> for &str {
+    #[inline]
+    fn eq(&self, other: &VarName) -> bool {
+        other == self
+    }
+}
+
+impl PartialEq<String> for VarName {
+    fn eq(&self, other: &String) -> bool {
+        self == other.as_str()
+    }
+}
+
+impl PartialEq<VarName> for String {
+    #[inline]
+    fn eq(&self, other: &VarName) -> bool {
+        other == self
+    }
+}
+
+impl PartialEq<&String> for VarName {
+    #[inline]
+    fn eq(&self, other: &&String) -> bool {
+        self.as_str() == *other
+    }
+}
+
+impl PartialEq<VarName> for &String {
+    #[inline]
+    fn eq(&self, other: &VarName) -> bool {
+        other == self
     }
 }
